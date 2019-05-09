2bf3b9c7dcc3d0d400807fa160539142ea153ff4
antonio <antonio@pykiss.com>
2019-05-09 12:35:25 +0200

    .

--- client/clip/administrarClip/administrarClip.js
+++ client/clip/administrarClip/administrarClip.js
@@ -48,3 +48,45 @@ Template.agregarMenu.helpers({
     return (ventanas.findOne('administrarClip') || {}).secreto
   }
 })
+
+Template.adminPost.events({
+  'click .cabecero .fa-bars' (event, template) {
+    template.$('.miniFondo').addClass('visible')
+  },
+  'click .miniFondo' (event, template) {
+    template.$('.miniFondo').removeClass('visible')
+  },
+  'click .establecerStatus' (event, template) {
+    event.stopPropagation()
+    const administrarClip = ventanas.findOne('administrarClip')
+    const clip = clips.findOne({
+      url: administrarClip.url
+    })
+    Meteor.call('establecerStatus', {
+      postId: this._id,
+      clipId: clip._id,
+      secreto: administrarClip.secreto,
+      status: event.currentTarget.dataset.status
+    }, ventanas.error)
+  },
+  'click .eliminar' () {
+    const administrarClip = ventanas.findOne('administrarClip')
+    const clip = clips.findOne({
+      url: administrarClip.url
+    })
+    Meteor.call('eliminarPost', {
+      postId: this._id,
+      clipId: clip._id,
+      secreto: administrarClip.secreto
+    }, ventanas.error)
+  }
+})
+
+Template.adminPost.helpers({
+  comprobarStatus (status) {
+    Template.currentData()
+    if (this.status === status) {
+      return true
+    }
+  }
+})
--- server/clip.js
+++ server/clip.js
@@ -7,14 +7,25 @@ import { tituloAUrl } from '/common/varios'
 import Joi from 'joi'
 
 const validaciones = {
+  establecerStatus: Joi.object().keys({
+    clipId: Joi.string().required(),
+    postId: Joi.string().required(),
+    secreto: Joi.string().required(),
+    status: Joi.string().valid(['RECHAZADO', 'OCULTO', 'VISIBLE']).required()
+  }),
+  eliminarPost: Joi.object().keys({
+    clipId: Joi.string().required(),
+    postId: Joi.string().required(),
+    secreto: Joi.string().required()
+  }),
   clipPublish: Joi.object().keys({
     url: Joi.string().regex(/^[a-z-]+$/).required(),
-    secreto: Joi.string()
+    secreto: Joi.string().required()
   }),
   agregarPost: Joi.object().keys({
     url: Joi.string().regex(/^[a-z-]+$/).required(),
-    link: Joi.string(),
-    rrss: Joi.string().valid(['facebook', 'instagram', 'twitter', 'youtube']),
+    link: Joi.string().required(),
+    rrss: Joi.string().valid(['facebook', 'instagram', 'twitter', 'youtube']).required(),
     secreto: Joi.string()
   }),
   titulo: Joi.string()
@@ -128,6 +139,68 @@ Meteor.methods({
     }).count()) {
       throw new Meteor.Error(400, 'url repetida')
     }
+  },
+  establecerStatus (opciones) {
+    salirValidacion({
+      data: opciones,
+      schema: validaciones.establecerStatus,
+      debug: {
+        donde: 'method establecerStatus'
+      }
+    })
+
+    clips.find({
+      _id: opciones.clipId
+    }).count() || salir(404, 'Clip no encontrado', {
+      donde: 'method establecerStatus'
+    })
+
+    clips.find({
+      _id: opciones.clipId,
+      secreto: opciones.secreto
+    }).count() || salir(400, 'No tienes permiso para administrar el clip', {
+      donde: 'method establecerStatus'
+    })
+
+    posts.find({
+      _id: opciones.postId,
+      clipId: opciones.clipId
+    }).count() || salir(404, 'Post no encontrado')
+
+    posts.update(opciones.postId, {
+      $set: {
+        status: opciones.status
+      }
+    })
+  },
+  eliminarPost (opciones) {
+    salirValidacion({
+      data: opciones,
+      schema: validaciones.eliminarPost,
+      debug: {
+        donde: 'method establecerStatus'
+      }
+    })
+
+    clips.find({
+      _id: opciones.clipId
+    }).count() || salir(404, 'Clip no encontrado', {
+      donde: 'method establecerStatus'
+    })
+
+    clips.find({
+      _id: opciones.clipId,
+      secreto: opciones.secreto
+    }).count() || salir(400, 'No tienes permiso para administrar el clip', {
+      donde: 'method establecerStatus'
+    })
+
+    posts.find({
+      _id: opciones.postId,
+      clipId: opciones.clipId
+    }).count() || salir(404, 'Post no encontrado')
+
+    posts.remove(opciones.postId)
   }
 })
 
