bedb685695a4c1f9615da8cc9cfbdac9131fe08f
antonio <antonio@pykiss.com>
2019-05-23 10:44:07 +0200

    actualizadas busquedas y ranking

--- server/listas.js
+++ server/listas.js
@@ -1,89 +1,31 @@
 import { Meteor } from 'meteor/meteor'
 import { links } from '/common/baseDeDatos'
 import { salirValidacion, validacionesComunes } from '/server/comun'
-import Joi from 'joi'
-
-const validaciones = {
-  primerPost: Joi.string(),
-  link: Joi.string()
-}
 
 Meteor.publish('ranking', function () {
-  return clips.find({
-    posts: {
-      $gt: 0
-    }
+  return links.find({
   }, {
-    fields: {
-      secreto: 0,
-      seguridad: 0
-    },
     sort: {
-      actualizacion: -1
+      votos: -1
     },
     limit: 100
   })
 })
 
 Meteor.publish('busqueda', function (busqueda) {
-  var regex = /(?:)/
-  try {
-    regex = new RegExp(busqueda)
-  } catch (e) {
-    regex = /$^/
-  }
-  return clips.find({
-    titulo: regex,
-    posts: {
-      $gt: 0
+  salirValidacion({
+    data: busqueda,
+    schema: validacionesComunes.texto
+  })
+  return links.find({
+    $text: {
+      $search: busqueda
     }
   }, {
     fields: {
-      secreto: 0,
-      seguridad: 0
+      score: { $meta: 'textScore' }
     },
-    sort: {
-      actualizacion: -1
-    },
-    limit: 100
-  })
-})
-
-Meteor.publish('primerPost', function (clipId) {
-  salirValidacion({
-    data: clipId,
-    schema: validaciones.primerPost,
-    debug: {
-      donde: 'publish clip'
-    }
-  })
-
-  return posts.find({
-    clipId,
-    status: 'VISIBLE'
-  }, {
-    sort: {
-      prioridad: -1,
-      timestamp: -1
-    },
-    limit: 1
-  })
-})
-
-Meteor.publish('postsDelLink', function (linkId) {
-  salirValidacion({
-    data: linkId,
-    schema: validacionesComunes._id
-  })
-  return posts.find({
-    linkId
-  }, {
-    fields: {
-      clipId: 1
-    },
-    sort: {
-      timestamp: -1
-    },
+    sort: { score: { $meta: 'textScore' } },
     limit: 100
   })
 })
